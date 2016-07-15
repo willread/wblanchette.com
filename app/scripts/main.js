fa07022169235149de7d20b703b125429bc0fa08
William Blanchette <wb@wblanchette.com>
2016-07-15 16:29:08 -0600

    Added counts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,29 @@
-console.log('\'Allo \'Allo!');
+// Fetch beer count
+
+$.ajax({
+  url: 'https://protected-bastion-41335.herokuapp.com/beers',
+  dataType: 'jsonp',
+  success: response => {
+    document.querySelector('[data-value="beers"]').innerText = response;
+  }
+});
+
+// Fetch game count
+
+$.ajax({
+  url: 'https://protected-bastion-41335.herokuapp.com/games',
+  dataType: 'jsonp',
+  success: response => {
+    document.querySelector('[data-value="games"]').innerText = response;
+  }
+});
+
+// Fetch song count
+
+$.ajax({
+  url: 'https://protected-bastion-41335.herokuapp.com/songs',
+  dataType: 'jsonp',
+  success: response => {
+    document.querySelector('[data-value="songs"]').innerText = response;
+  }
+});
