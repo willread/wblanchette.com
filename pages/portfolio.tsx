1b3f4c75e3117c8aaa412cf1a4706e4699fdd45f
Will Read <will@willread.ca>
2021-08-02 15:17:28 -0600

    Disable portfolio component

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,126 +1,126 @@
-import  React from 'react';
+// import  React from 'react';
 
-import Layout from '../components/Layout';
+// import Layout from '../components/Layout';
 
-import './portfolio.scss';
+// import './portfolio.scss';
 
-type PortfolioProps = {
-  portfolio: any
-};
+// type PortfolioProps = {
+//   portfolio: any
+// };
 
-type DeviceType = 'desktop'|'mobile';
+// type DeviceType = 'desktop'|'mobile';
 
-type PortfolioState = {
-  activePages: number[];
-  activeDevices: DeviceType[];
-  fullscreenSite: number|null;
-}
+// type PortfolioState = {
+//   activePages: number[];
+//   activeDevices: DeviceType[];
+//   fullscreenSite: number|null;
+// }
 
-export default class extends React.Component<PortfolioProps>{
-  state: PortfolioState;
+// export default class extends React.Component<PortfolioProps>{
+//   state: PortfolioState;
 
-  static async getInitialProps() {
-    return {
-      portfolio: await import('../src/portfolio.json')
-    };
-  }
+//   static async getInitialProps() {
+//     return {
+//       portfolio: await import('../src/portfolio.json')
+//     };
+//   }
 
-  constructor(props) {
-    super(props);
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      activePages: this.props.portfolio.sites.map(site => 0),
-      activeDevices: this.props.portfolio.sites.map(site => 'desktop'),
-      fullscreenSite: null
-    };
-  }
+//     this.state = {
+//       activePages: this.props.portfolio.sites.map(site => 0),
+//       activeDevices: this.props.portfolio.sites.map(site => 'desktop'),
+//       fullscreenSite: null
+//     };
+//   }
 
-  changePage(siteIndex: number, pageIndex: number) {
-    const activePages = [...this.state.activePages];
+//   changePage(siteIndex: number, pageIndex: number) {
+//     const activePages = [...this.state.activePages];
 
-    activePages[siteIndex] = pageIndex;
+//     activePages[siteIndex] = pageIndex;
 
-    this.setState({ activePages });
-  }
+//     this.setState({ activePages });
+//   }
 
-  toggleFullscreen(siteIndex: number, e?: MouseEvent) {
-    if (e && siteIndex === this.state.fullscreenSite) {
-      e.preventDefault();
-      return false;
-    }
+//   toggleFullscreen(siteIndex: number, e?: MouseEvent) {
+//     if (e && siteIndex === this.state.fullscreenSite) {
+//       e.preventDefault();
+//       return false;
+//     }
 
-    this.setState({ fullscreenSite: siteIndex });
+//     this.setState({ fullscreenSite: siteIndex });
 
-    if (typeof siteIndex === 'number') {
-      document.body.style.overflow = 'hidden';
-    }else {
-      document.body.style.overflow = '';
-    }
-  }
+//     if (typeof siteIndex === 'number') {
+//       document.body.style.overflow = 'hidden';
+//     }else {
+//       document.body.style.overflow = '';
+//     }
+//   }
 
-  changeDevice(siteIndex: number, device: DeviceType) {
-    const activeDevices = [...this.state.activeDevices];
+//   changeDevice(siteIndex: number, device: DeviceType) {
+//     const activeDevices = [...this.state.activeDevices];
 
-    activeDevices[siteIndex] = device;
+//     activeDevices[siteIndex] = device;
 
-    this.setState({ activeDevices } );
-  }
+//     this.setState({ activeDevices } );
+//   }
 
-  render() {
-    return (
-      <Layout>
-        <section className={`portfolio ${typeof this.state.fullscreenSite === 'number' ? 'fullscreen' : ''}`}>
-          <div className='sites'>
-            {this.props.portfolio.sites.map((site, siteIndex) => (
-              <div
-                key={siteIndex}
-                className={`site ${this.state.fullscreenSite === siteIndex ? 'fullscreen' : ''}`}
-              >
-                <h1>{site.title}</h1>
-                <p>{site.description}</p>
-                {site.stack &&
-                  <div className="stack">
-                    {site.stack.map(tech => (<li key={tech}>{tech}</li>))}
-                  </div>
-                }
-                {site.pages.map((page, pageIndex) => (
-                  <div
-                    key={pageIndex}
-                    className={`page ${this.state.activePages[siteIndex] === pageIndex ? 'active' : ''}`}
-                    onClick={e => this.toggleFullscreen(siteIndex, e)}
-                  >
-                    <div className='overlay' onClick={() => this.toggleFullscreen(null)}></div>
-                    {page.screenshots.map((screenshot, screenshotIndex) => (
-                      <div
-                        key={screenshotIndex}
-                        className={`screenshot ${screenshot.device} ${this.state.activeDevices[siteIndex] === screenshot.device ? 'active' : ''}`}
-                        onClick={() => this.changeDevice(siteIndex, screenshot.device)}
-                      >
-                        <div className='inner'>
-                          <img src={`/static/images/portfolio/${screenshot.path}`}/>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                ))}
+//   render() {
+//     return (
+//       <Layout>
+//         <section className={`portfolio ${typeof this.state.fullscreenSite === 'number' ? 'fullscreen' : ''}`}>
+//           <div className='sites'>
+//             {this.props.portfolio.sites.map((site, siteIndex) => (
+//               <div
+//                 key={siteIndex}
+//                 className={`site ${this.state.fullscreenSite === siteIndex ? 'fullscreen' : ''}`}
+//               >
+//                 <h1>{site.title}</h1>
+//                 <p>{site.description}</p>
+//                 {site.stack &&
+//                   <div className="stack">
+//                     {site.stack.map(tech => (<li key={tech}>{tech}</li>))}
+//                   </div>
+//                 }
+//                 {site.pages.map((page, pageIndex) => (
+//                   <div
+//                     key={pageIndex}
+//                     className={`page ${this.state.activePages[siteIndex] === pageIndex ? 'active' : ''}`}
+//                     onClick={e => this.toggleFullscreen(siteIndex, e)}
+//                   >
+//                     <div className='overlay' onClick={() => this.toggleFullscreen(null)}></div>
+//                     {page.screenshots.map((screenshot, screenshotIndex) => (
+//                       <div
+//                         key={screenshotIndex}
+//                         className={`screenshot ${screenshot.device} ${this.state.activeDevices[siteIndex] === screenshot.device ? 'active' : ''}`}
+//                         onClick={() => this.changeDevice(siteIndex, screenshot.device)}
+//                       >
+//                         <div className='inner'>
+//                           <img src={`/static/images/portfolio/${screenshot.path}`}/>
+//                         </div>
+//                       </div>
+//                     ))}
+//                   </div>
+//                 ))}
 
-                {site.pages.length > 1 &&
-                  <nav>
-                    {site.pages.map((page, index) => (
-                      <button
-                        key={index}
-                        onClick={() => this.changePage(siteIndex, index)}
-                        className={this.state.activePages[siteIndex] === index ? 'active' : ''}
-                      ></button>
-                    ))}
-                  </nav>
-                }
-              </div>
-            ))}
-          </div>
-          <button className='close' onClick={() => this.toggleFullscreen(null)}>X</button>
-        </section>
-      </Layout>
-    );
-  }
-};
+//                 {site.pages.length > 1 &&
+//                   <nav>
+//                     {site.pages.map((page, index) => (
+//                       <button
+//                         key={index}
+//                         onClick={() => this.changePage(siteIndex, index)}
+//                         className={this.state.activePages[siteIndex] === index ? 'active' : ''}
+//                       ></button>
+//                     ))}
+//                   </nav>
+//                 }
+//               </div>
+//             ))}
+//           </div>
+//           <button className='close' onClick={() => this.toggleFullscreen(null)}>X</button>
+//         </section>
+//       </Layout>
+//     );
+//   }
+// };
